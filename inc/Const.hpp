f2ed6360f975782f40553752269fc33423c85bf2
makelannika <makelannika@gmail.com>
2024-12-04 19:21:56 +0200

    upacket_size

--- inc/Const.hpp
+++ inc/Const.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
-static const unsigned int BUFFER_SIZE = 1024;
-static const unsigned int PACKET_SIZE = 8192; // 8 KB
-static const unsigned int CLIENT_TIMEOUT = 5000;
+static const unsigned int	BUFFER_SIZE = 1024;
+static const size_t			PACKET_SIZE = 8192; // 8 KB
+static const unsigned int	UPACKET_SIZE = 8192;
+static const unsigned int	CLIENT_TIMEOUT = 5000;
