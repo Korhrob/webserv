a63e4532b9fbb88ece2f44a26b64ed79d6986134
Annika Mäkelä <makelannika@gmail.com>
2024-12-06 23:09:19 +0200

    parsing

--- src/Response.cpp
+++ src/Response.cpp
@@ -80,12 +80,13 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 		return;
 	}
 	// {
-	std::istringstream	first_line(m_request.substr(0, pos));
+	std::istringstream	request_line(m_request.substr(0, pos));
 	std::string			version;
 	std::string			method;
+	std::string			rest;
 
-	if (!(first_line >> method >> m_path >> version) || version != "HTTP/1.1") { // what if there's stuff after version?
-		logError("");
+	if (!(request_line >> method >> m_path >> version) || request_line >> rest || version != "HTTP/1.1") {
+		logError("Invalid request line");
 		return;
 	}
 
@@ -93,7 +94,7 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 	if (methods.find(method) != methods.end()) {
 		m_method = methods[method];
 	} else {
-		logError("");
+		logError("Invalid or missing method");
 		return;
 	}
 	// }
@@ -107,24 +108,25 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 		std::string	key = line.substr(0, pos);
 		std::string	value(pos + 1 != std::string::npos ? line.substr(pos + 1) : ""); // should this be an error?
 		key.erase(key.find_last_not_of(" \t") + 1);
+		std::transform(key.begin(), key.end(), key.begin(), [](unsigned char c){ return std::toupper(c); });
+		std::replace(key.begin(), key.end(), '-', '_');
 		value.erase(0, value.find_first_not_of(" \t"));
 		value.erase(value.find_last_not_of("\r") + 1);
 		m_headers.try_emplace(key, value);
 	}
 
-	m_connection = m_headers.find("Connection") != m_headers.end() && m_headers["Connection"] == "keep-alive" ? true : false;
+	m_connection = m_headers.find("CONNECTION") != m_headers.end() && m_headers["CONNECTION"] == "keep-alive" ? true : false;
 
-	m_send_type = m_headers.find("Transfer-Encoding") != m_headers.end() && m_headers["Transfer-Encoding"] == "chunked" ? CHUNK : SINGLE;
+	m_send_type = m_headers.find("TRANSFER_ENCODING") != m_headers.end() && m_headers["TRANSFER_ENCODING"] == "chunked" ? CHUNK : SINGLE;
 
-	m_size = m_headers.find("Content-Length") != m_headers.end() ? std::stoul(m_headers["Content-Length"]) : 0;
+	m_size = m_headers.find("CONTENT_LENGTH") != m_headers.end() ? std::stoul(m_headers["CONTENT_LENGTH"]) : 0;
 
+	// if path starts with /cgi-bin/ or ends with .cgi -> invoke CGI handler (GET,POST)
 	m_path = m_path.substr(1); // do this better might throw exception
 	if (m_method == GET) {
 		if (m_path.empty())
 				m_path = "index.html";
 
-			// test for 'path', 'path.html', 'path/index.html' 
-			// what if path is empty
 		const std::vector<std::string> alt { ".html", "/index.html" };
 
 		std::ifstream file(m_path);
@@ -143,7 +145,6 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 				}
 			}
 		}
-
 		try {
 			m_size = std::filesystem::file_size(m_path);
 		} catch (const std::exception& e) {
@@ -152,12 +153,12 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 		}
 	}
 
-	std::cout << "-- PARSED ---------------------------------------------\n"
-	<< method << " " << m_path << " " << version << "\n";
-	for (const auto& [key, value] : m_headers) {
-        std::cout << key << ":" << value << "\n";
-    }
-	std::cout << "-------------------------------------------------------\n";
+	// std::cout << "-- PARSED ---------------------------------------------\n"
+	// << method << " " << m_path << " " << version << "\n";
+	// for (const auto& [key, value] : m_headers) {
+    //     std::cout << key << ":" << value << "\n";
+    // }
+	// std::cout << "-------------------------------------------------------\n";
 	/*
 		check for content-disposition -> name="name" -> open file with the name
 		if chuncked set output_filestream for client
@@ -166,22 +167,20 @@ void	Response::parseRequest(std::shared_ptr<Client> client)
 	*/
 
 	if (m_method == POST) {
-		/*
-			depending on Content-Type
-				application/x-www-form-urlencoded:
-					Parse key-value pairs from the body to a map
-				multipart/form-data:
-					Used for file uploads, parse boundary-separated sections CGI?
-				application/json:
-					Parse the body as JSON using a library or custom parser
-		*/
-		if (m_headers.find("Content-Type") != m_headers.end() && m_headers["Content-Type"] == "application/x-www-form-urlencoded") {
-			while (getline(request, line, '&')) {
-				pos = line.find('=');
-				if (pos != std::string::npos)
-					client->setFormData(line.substr(0, pos), line.substr(pos + 1));
+		if (m_headers.find("CONTENT_TYPE") != m_headers.end()) { // has a body to parse
+			if (m_headers["CONTENT_TYPE"] == "application/x-www-form-urlencoded") {
+				// Parse key-value pairs from the body to a map
+				while (getline(request, line, '&')) {
+					pos = line.find('=');
+					if (pos != std::string::npos)
+						client->setFormData(line.substr(0, pos), line.substr(pos + 1));
+				}
+			} else if (m_headers["CONTENT_TYPE"].find("multipart/form-data") != std::string::npos) {
+				// file uploads, parse boundary-separated sections, CGI?
+			} else if (m_headers["CONTENT_TYPE"] == "application/json") {
+				// Parse the body as JSON using a library or custom parser
 			}
-		}
+		} 
 	}
 
 	if (m_method == DELETE) {
