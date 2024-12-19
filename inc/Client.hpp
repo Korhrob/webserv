32184e0ee4789e9115cacf48d306d27cde828151
makelannika <makelannika@gmail.com>
2024-12-19 17:03:44 +0200

    merging

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -153,6 +153,7 @@ class Client
 			// auto now = std::chrono::steady_clock::now();
 			// auto stamp = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 			m_file.open(std::to_string(m_id) + "_" + name, std::ios::out | std::ios::app | std::ios::binary);
+			
 			return m_file;
 		}
 		
