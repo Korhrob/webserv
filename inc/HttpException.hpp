2ea31c5eaff54b44d52d72f8fe330aaf7ac0fa5f
makelannika <makelannika@gmail.com>
2025-01-07 17:22:32 +0200

    exception messages fixed

--- inc/HttpException.hpp
+++ inc/HttpException.hpp
@@ -7,14 +7,13 @@ class HttpException : public std::exception {
     private:
         int         m_statusCode;
         std::string m_msg;
-        std::string m_log;
 
     public:
-        HttpException(int code, std::string msg, std::string log)
-            : m_statusCode(code), m_msg(msg), m_log(log) {}
+        HttpException(int code, std::string msg)
+            : m_statusCode(code), m_msg(msg) {}
 
         const char* what() const noexcept override {
-            return m_log.c_str();
+            return m_msg.c_str();
         }
 
         int getStatusCode() const noexcept {
@@ -22,26 +21,26 @@ class HttpException : public std::exception {
         }
 
         static HttpException    badRequest(std::string log) {
-            return HttpException(400, "Bad Request", log);
+            return HttpException(400, "Bad Request: " + log);
         }
 
         static HttpException    notFound() {
-            return HttpException(404, "Not Found", "");
+            return HttpException(404, "Not Found");
         }
 
 		static HttpException	lengthRequired() {
-			return HttpException(411, "Length Required", "");
+			return HttpException(411, "Length Required");
 		}
 
         static HttpException    unsupportedMediaType() {
-            return HttpException(415, "Unsupported Media Type", "");
+            return HttpException(415, "Unsupported Media Type");
         }
 
         static HttpException    notImplemented() {
-            return HttpException(501, "Not Implemented", "");
+            return HttpException(501, "Not Implemented");
         }
 
         static HttpException    httpVersionNotSupported() {
-            return HttpException(505, "HTTP Version Not Supported", "");
+            return HttpException(505, "HTTP Version Not Supported");
         }
 };
