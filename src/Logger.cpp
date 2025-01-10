a4f8457f9322165104f2a6c6f36a566163def004
Korhrob <korhrob@gmail.com>
2025-01-10 15:29:13 +0200

    missing Logger class files

---
+++ src/Logger.cpp
@@ -0,0 +1,20 @@
+#include "Logger.hpp"
+
+#include <string>
+#include <iostream>
+#include <ostream>
+#include <fstream>
+
+void	Logger::log(const std::string& msg)
+{
+	if (m_enabled)
+		m_log_file << msg << std::endl;
+	//std::cout << msg << std::endl;
+}
+
+void	Logger::logError(const std::string& msg)
+{
+	if (m_enabled)
+		m_log_error << msg << std::endl;
+	//std::cout << "Error: " << msg << std::endl;
+}
