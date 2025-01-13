e3f3465b13efbf10dca1e68920bacbc42d176e72
Korhrob <korhrob@gmail.com>
2025-01-13 09:58:29 +0200

    added ifndef for defines

--- inc/Const.hpp
+++ inc/Const.hpp
@@ -6,8 +6,13 @@
 static const unsigned int BUFFER_SIZE = 1024;
 static const unsigned int PACKET_SIZE = 8192; // 8 KB
 static const unsigned int CLIENT_TIMEOUT = 5000; // 5 seconds
+
+#ifndef LOG_ENABLE
 # define LOG_ENABLE false
+#endif
+#ifndef LOG_STROUT
 # define LOG_STDOUT true
+#endif
 
 const std::string				WHITESPACE = " \t";
 const std::vector<std::string>	EMPTY_VECTOR;
