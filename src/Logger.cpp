2175ace07d89052d17895f1a2abc01e50af858c7
Korhrob <korhrob@gmail.com>
2025-01-28 21:25:06 +0200

    added timestamps to logs

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -4,21 +4,30 @@
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
 
 void	Logger::log(const std::string& msg)
 {
+	std::time_t	now_time = std::time(nullptr);
+	std::tm*	local_time = std::localtime(&now_time);
+
 	if (m_enabled)
-		m_log_file << msg << std::endl;
+		m_log_file << std::put_time(local_time, "[%H:%M:%S] ") << msg << std::endl;
 	#ifdef LOG_STDOUT
-	std::cout << msg << std::endl;
+	std::cout << std::put_time(local_time, "[%H:%M:%S] ") << msg << std::endl;
 	#endif
 }
 
 void	Logger::logError(const std::string& msg)
 {
+	std::time_t	now_time = std::time(nullptr);
+	std::tm*	local_time = std::localtime(&now_time);
+
 	if (m_enabled)
-		m_log_error << msg << std::endl;
+		m_log_error << std::put_time(local_time, "[%H:%M:%S] ") << msg << std::endl;
 	#ifdef LOG_STDOUT
-		std::cout << "Error: " << msg << std::endl;
+		std::cout << std::put_time(local_time, "[%H:%M:%S] ") << "Error: " << msg << std::endl;
 	#endif
 }
