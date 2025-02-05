4adf0062fbd1aab0978b5d86453aa555d7fb3136
makelannika <makelannika@gmail.com>
2025-02-05 19:12:27 +0200

    added flag for connection: close & fixed setting headers

--- inc/HttpResponse.hpp
+++ inc/HttpResponse.hpp
@@ -7,10 +7,8 @@
 
 enum	e_type
 {
-	TYPE_BLANK,
 	TYPE_SINGLE,
-	TYPE_CHUNKED,
-	TYPE_NONE
+	TYPE_CHUNKED
 };
 
 class HttpResponse {
@@ -19,18 +17,22 @@ class HttpResponse {
 		std::string										m_msg;
 		std::unordered_map<std::string, std::string>	m_headers;
 		std::string										m_body;
-		e_type											m_sendType;
+		e_type											m_type;
+		bool											m_close;
 
 		std::string	getStatusLine();
 		std::string	getHeaders();
 		std::string	getBody(const std::string& path);
 		
+		void	setHeaders();
+		
 	public:
 		HttpResponse(int code, const std::string& msg);
 		HttpResponse(int code, const std::string& msg, const std::string& path);
 
+
 		std::string	getResponse();
 		std::string	getHeader();
 		e_type		getSendType();
-		int			getStatusCode();
+		bool		closeConnection();
 };
