0d204faafe244d34564dc1a0445de5c6b10fb10e
makelannika <makelannika@gmail.com>
2025-02-07 13:53:57 +0200

    removed unnecessary files, logic moved to HttpResponse

--- inc/Parse.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string	getBody(const std::string& path);
-std::string	getHeaderSingle(const size_t& len, int ecode, std::string msg);
-std::string	getHeaderChunk();
